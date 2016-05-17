50aada757d79f32910a9b7a3fff161e63981a683
estai <estai@estai-hymath>
2016-05-18 01:12:43 +0900

    EST64OS source -version 0.1 very simple boot loader

---
+++ 00.BootLoader/BootLoader.asm
@@ -0,0 +1,19 @@
+[ORG 0x00]
+[BITS 16]
+
+SECTION .text
+
+mov ax, 0xB800
+mov ds, ax
+
+mov byte[0x00], 'M'	; 'M' in DS:0x0000
+mov byte[0x01], 0x0A	; 0x0A in DS:0x0001
+
+jmp $					; 아직 더 이상 로드할 이미지가 없으므로 임시로 여기서 무한루프
+
+; 0 padding for boot loader(512byte)
+times 510 - ($ - $$)	db 0x00
+
+; signature for boot loader ( dw 0xAA55 )
+db 0x55
+db 0xAA
